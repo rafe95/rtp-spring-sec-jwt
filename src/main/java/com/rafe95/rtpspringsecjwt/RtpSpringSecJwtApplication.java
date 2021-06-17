410e481732ada69bf7a90e73d85a688d07ebfa98
rafe <rafe.estevez@gmail.com>
2021-06-17 00:27:35 -0400

    1st commit

---
+++ src/main/java/com/rafe95/rtpspringsecjwt/RtpSpringSecJwtApplication.java
@@ -0,0 +1,13 @@
+package com.rafe95.rtpspringsecjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtpSpringSecJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtpSpringSecJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafe95/rtpspringsecjwt/RtpSpringSecJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafe95.rtpspringsecjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtpSpringSecJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
